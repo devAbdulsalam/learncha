e438fd78444b46206808bfa9d16a540868ee4acb
devAbdulsalam <ammuftau74@gmail.com>
2024-05-04 05:47:20 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = ({id}) => {
+  return (
+    <footer className="w-full bg-[#85d8e5]">
+        <div className="">
+            <div className="footer-frame">
+                <div className="flex justify-center w-full">
+                    <div className="text-center text-white font-bold p-6">
+                        <p>Copyright 2022 Learncha. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
